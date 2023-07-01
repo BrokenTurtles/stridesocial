c11115d8a76c1914bfa7292180accfb2011fc977
Eric Dalio <ericdalio@gmail.com>
2023-07-01 16:02:38 -0400

    Regiser new users complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import Register from './Register';
 
 const App = () => {
+  const [message, setMessage] = useState('');
 
-const [message, setMessage] = useState('')
-
-useEffect(() => {
-    fetch('/api')
-      .then(response => response.json())
-      .then(data => setMessage(data.message));
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api')
+  //     .then((response) => response.json())
+  //     .then((data) => setMessage(data.message));
+  // }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        TEST
+    <div className='App'>
+      <header className='App-header'>
+        <Register />
         {message}
       </header>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- server/config/db.js
+++ server/config/db.js
@@ -4,17 +4,20 @@ import mongoose from 'mongoose';
 //dotenv.config()
 
 const connectDB = async () => {
-	try {
-		console.log('MONGO_URI', process.env.MONGO_URI)
-		const connection = await mongoose.connect("mongodb+srv://brok3turtl3:admin@cluster0.0e2wjxf.mongodb.net/", {
-      useUnifiedTopology: true,
-      useNewUrlParser: true
-    });
-		console.log(`MongoDB is connected to: ${connection.connection.host}`);
-	} catch (error) {
-		console.log(error.message);
-		process.exit(1);
-	}
+  try {
+    //console.log('MONGO_URI', process.env.MONGO_URI)
+    const connection = await mongoose.connect(
+      'mongodb+srv://brok3turtl3:admin@cluster0.0e2wjxf.mongodb.net/',
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
+    console.log(`MongoDB is connected to: ${connection.connection.host}`);
+  } catch (error) {
+    console.log(error.message);
+    process.exit(1);
+  }
 };
 
-export default connectDB
+export default connectDB;
