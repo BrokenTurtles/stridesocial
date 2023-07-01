3ede080ece9eb3c0b047100374b38e41fe86f59e
Paul Glenn <nls.pglenn@gmail.com>
2023-07-01 16:47:46 -0400

    restored addUserInterests

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -58,4 +58,32 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-export { registerUser, authUser };
+const addUserInterests = asyncHandler(async (req, res) => {	
+	// find user by email 
+	// interests should be an array 
+	const {email, interests} = req.body;
+
+
+	const user = await User.findOne({email: email}); 
+
+	try {
+		// add interests to user interests property 
+		user.interests.push(...interests);
+		// save document 
+		user.save() ;
+		res.status(200).json(user); 
+	} catch {
+		res.status(401);  
+		throw new Error('Invalid email'); 
+	}
+
+
+
+
+})
+
+export {
+	registerUser,
+    authUser,
+		addUserInterests
+};
