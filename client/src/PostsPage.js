5e03ffe7c25abbd435a96cf7d07fe9b98597534a
Eric Dalio <ericdalio@gmail.com>
2023-07-03 12:22:04 -0400

    postDelete done

--- client/src/PostsPage.js
+++ client/src/PostsPage.js
@@ -1,32 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
-const Post = ({ post }) => (
-  <div className="post">
+const Post = ({ post, handleDelete }) => (
+  <div className='post'>
     <h3>{post.name}</h3>
     <p>{post.body}</p>
+    <button id={post._id} onClick={handleDelete}>
+      Delete Post
+    </button>
   </div>
 );
-
-
 
 const PostsPage = () => {
   const [message, setMessage] = useState('');
   const [posts, setPosts] = useState();
-  const [user, setUser] = useState()
+  const [user, setUser] = useState();
+  const [deletedPost, setDeletedPost] = useState();
 
   const getAllPosts = async () => {
     try {
+      console.log('trying to get all posts');
       const data = await axios.get('api/posts');
-      console.log(data)
-      setPosts(data.data)
-      console.log(posts)
-    } catch(error) {
-      console.log(error)
+      console.log(data);
+      setPosts(data.data);
+      console.log(posts);
+    } catch (error) {
+      console.log(error);
     }
-  }
-
+  };
 
   useEffect(() => {
     const storedUser = localStorage.getItem('user');
@@ -35,57 +36,78 @@ const PostsPage = () => {
     }
   }, []);
 
-
   useEffect(() => {
-   
     getAllPosts();
   }, []);
 
-
   const handleSubmit = async (event) => {
     event.preventDefault();
-    try{
+    try {
       const config = {
-        headers:{
-          'Content-Type': 'application/json'
-        }
-      }
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
 
       const data = {
         userId: user._id,
         name: user.name,
-        body: message
-      }
+        body: message,
+      };
 
-      const response = await axios.post('api/posts', data, config)
-      setPosts([response.data, ...posts])
-    }catch(error){
-      console.log(error.message)
+      const response = await axios.post('api/posts', data, config);
+      setPosts([response.data, ...posts]);
+    } catch (error) {
+      console.log(error.message);
     }
   };
 
-  
-  
+  const handleDelete = async (event) => {
+    console.log('handleDelete');
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      };
+
+      const data = {
+        userId: user._id,
+      };
+
+      const response = await axios.delete(
+        'api/posts/' + event.target.id,
+        data,
+        config
+      );
+      getAllPosts();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
 
   return (
-    <div className="posts-container">
-      <form className="post-form" onSubmit={handleSubmit}>
+    <div className='posts-container'>
+      <form className='post-form' onSubmit={handleSubmit}>
         <input
-          type="text"
+          type='text'
           value={message}
           onChange={(e) => setMessage(e.target.value)}
-          placeholder="Enter your message"
-          className="post-input"
+          placeholder='Enter your message'
+          className='post-input'
         />
-        <button type="submit" className="post-button">Submit</button>
+        <button type='submit' className='post-button'>
+          Submit
+        </button>
       </form>
-      <div className="posts-display">
-  {Array.isArray(posts) && posts.map((post) => (
-    <Post key={post._id} post={post} />
-  ))}
-</div>
+      <div className='posts-display'>
+        {Array.isArray(posts) &&
+          posts.map((post) => (
+            <Post key={post._id} post={post} handleDelete={handleDelete} />
+          ))}
+      </div>
     </div>
   );
-}
+};
 
 export default PostsPage;
