87a62c8ce2cd83d40fa947d11cd4952532918384
Paul Glenn <nls.pglenn@gmail.com>
2023-07-03 10:57:47 -0400

    email validation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,19 +1,26 @@
-import asyncHandler from 'express-async-handler';
-import User from '../models/userModel.js';
-import generateToken from '../utils/generateToken.js';
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import generateToken from "../utils/generateToken.js";
 
 //ENDPOINT  POST api/users
 //PURPOSE   Register a new user
 //ACCESS    Public
 const registerUser = asyncHandler(async (req, res) => {
-  console.log('RegUser hit!!!');
+  console.log("RegUser hit!!!");
   const { name, email, password } = req.body;
 
   const userExists = await User.findOne({ email });
 
   if (userExists) {
     res.status(400);
-    throw new Error('User already exists');
+    throw new Error("User already exists");
+  }
+
+  const isValidEmail = email.match(/[\w\d]+@[a-z]+\.[\w]+$/gim);
+
+  if (!isValidEmail) {
+    res.status(400);
+    throw new Error("Invalid email!");
   }
 
   const user = await User.create({
@@ -30,8 +37,9 @@ const registerUser = asyncHandler(async (req, res) => {
       token: generateToken(user._id),
     });
   } else {
+    console.log("already exists?");
     res.status(400);
-    //throw new Error('Invalid user data');
+    //throw new Error("Invalid user data");
   }
 });
 
@@ -54,34 +62,27 @@ const authUser = asyncHandler(async (req, res) => {
     });
   } else {
     res.status(401);
-    throw new Error('Invalid email or password');
+    throw new Error("Invalid email or password");
   }
 });
 
-const addUserInterests = asyncHandler(async (req, res) => {	
-	// find user by email 
-	// interests should be an array 
-	const {email, interests} = req.body;
+const addUserInterests = asyncHandler(async (req, res) => {
+  // find user by email
+  // interests should be an array
+  const { email, interests } = req.body;
 
+  const user = await User.findOne({ email: email });
 
-	const user = await User.findOne({email: email}); 
+  try {
+    // add interests to user interests property
+    user.interests.push(...interests);
+    // save document
+    user.save();
+    res.status(200).json(user);
+  } catch {
+    res.status(401);
+    throw new Error("Invalid email");
+  }
+});
 
-	try {
-		// add interests to user interests property 
-		user.interests.push(...interests);
-		// save document 
-		user.save() ;
-		res.status(200).json(user); 
-	} catch {
-		res.status(401);  
-		throw new Error('Invalid email'); 
-	}
-
-	
-})
-
-export {
-	registerUser,
-    authUser,
-		addUserInterests
-};
+export { registerUser, authUser, addUserInterests };
