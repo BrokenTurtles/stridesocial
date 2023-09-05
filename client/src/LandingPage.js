91850abbd919f862ed68e8aa6b2b7bd6ee68a073
james <jamesli0226@gmail.com>
2023-09-05 04:34:50 -0400

    test

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -1,37 +1,54 @@
-import React, { useState } from 'react';
-import Register from './Register.js';
-import Login from './Login.js';
+import React, { useState } from "react";
+import Register from "./Register.js";
+import Login from "./Login.js";
 
+export default function LandingPage() {
+  const [register, setRegister] = useState(false);
+  const [login, setLogin] = useState(false);
 
-export default function LandingPage () {
-    const [register, setRegister] = useState(false)
-    const [login, setLogin] = useState(false)
+  const [count, setCount] = useState(0);
 
-    return (
+  const handleClick = () => {
+    setCount(count + 1);
+    console.log(count);
+  };
 
-        <main>
-            <h1 className="header">Welcome to StrideSocial! </h1>
-            <div className='options'>
-                <button className="" onClick={() => {
-                    setRegister(true);
-                    setLogin(false);
-                    }}>Register</button>
-                <button className="" onClick={() => {
-                    setLogin(true);
-                    setRegister(false);
-                    }}>Login </button>
-            </div>
-            <div>
-                <div className='register'>
-                    {register===true && login === false ? <Register /> : null }
-                </div>
-                <div className='login'>
-                    {login=== true && register === false ? <Login /> : null }
-                </div>
-            </div>
-
-        </main>
-
-    )
-
+  return (
+    <>
+      <div>
+        <button onClick={() => handleClick()}> Here {count}</button>
+      </div>
+      <main>
+        <h1 className="header">Welcome to StrideSocial! </h1>
+        <div className="options">
+          <button
+            className=""
+            onClick={() => {
+              setRegister(true);
+              setLogin(false);
+            }}
+          >
+            Register
+          </button>
+          <button
+            className=""
+            onClick={() => {
+              setLogin(true);
+              setRegister(false);
+            }}
+          >
+            Login{" "}
+          </button>
+        </div>
+        <div>
+          <div className="register">
+            {register === true && login === false ? <Register /> : null}
+          </div>
+          <div className="login">
+            {login === true && register === false ? <Login /> : null}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
